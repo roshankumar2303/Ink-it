50618ddd3494e83860d813e66ab931322832218d
[roshankumar] <58880369+roshankumar2303@users.noreply.github.com>
2021-06-28 23:02:44 +0530

    Modified the class Labels

--- src/com/inkit/program/Labels.java
+++ src/com/inkit/program/Labels.java
@@ -1,20 +1,23 @@
 package com.inkit.program;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.Hashtable;
 
 public class Labels {
-    static LinkedHashSet<String> allLabels;
+    // KEY: String -> Label
+    // VAL: Integer[] -> uniqueIDs of all the notes under that label
+    static Hashtable<String, Integer[]> allLabels;
 
     static {
         // [INCOMPLETE] This static block runs for the first time when "Labels" class has been invoked through a static method
-        // [INCOMPLETE] Read all the labels from the file and store it in "allLabels"
-        allLabels = new LinkedHashSet<>(); // --->> JUST A PLACEHOLDER FOR NOW
+        // [INCOMPLETE] Retrieve all the labels from the file and store it in "allLabels"
+        // Hashtable is serializable. Not sure about Integer[] tho. But I guess we can write Hashtable object to the file, and it can be retrieved -- Roshan
+        allLabels = new Hashtable<>(); // --->> JUST A PLACEHOLDER FOR NOW
 
     }
 
     /**
-     * This static method helps in choosing labels from pre-existing labels.
+     * This static method helps in choosing labels from pre-existing labels stored in a file as a {@code Hashtable} object.
      * @param uniqueID The unique ID of a note whose labels are being chosen
      * @return  ArrayList of chosen Labels
      */
@@ -24,11 +27,12 @@ public class Labels {
         // Printing all the labels for the user to choose
         int i = 0;
         System.out.println("-- CHOOSE LABELS FROM THE BELOW ONES ----------");
-        for(String label: allLabels) {
+        for(String label: allLabels.keySet()) {
             System.out.println((i++) + " --> " + label);
         }
 
-        // [INCOMPLETE] Choosing labels, and adding those to selected labels
+        // [INCOMPLETE] Choosing labels, and adding those to selectedLabels, and Updating allLabels to include uniqueID of the current new note
+
 
         // Returning Selected Labels
         return selectedLabels;
